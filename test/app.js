03367a270a65c66c08270fef1a7f062432c3ae08
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-07-12 13:09:28 -0400

    mlab update

--- test/app.js
+++ test/app.js
@@ -6,9 +6,9 @@ app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
+if(process.env.MLAB_USERNAME_WEBDEV_NEW) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV_NEW; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV_NEW;
     connectionString = 'mongodb://' + username + ':' + password;
     connectionString += '@ds137139.mlab.com:37139/heroku_d64prf40'; // user yours
 }
