f8264fd318ab0c29dc4c8f2f0727acfe86735d7d
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-07-25 15:57:13 -0400

    assignemnt 3 final update

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -1,81 +1,82 @@
 (function () {
-    angular
-        .module('WamApp')
-        .factory('userService', userService);
 
-    function userService() {
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder",  email: "alice@abc.com", dob:""},
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley", email: "bob@abc.com", dob:""  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia", email: "charly@abc.com", dob:""  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi", email: "jannuzi@abc.com", dob:"" }
-        ];
+angular
+    .module('WamApp')
+    .factory('userService', userService);
 
-        var api ={
-            registerUser: registerUser,
-            findUserById: findUserById,
-            findUserbyUsername: findUserbyUsername,
-            findUserByUsernameAndPassword : findUserByUsernameAndPassword,
-            updateUser: updateUser,
-            deleteUser:deleteUser
-        };
+function userService() {
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder",  email: "alice@abc.com", dob:""},
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley", email: "bob@abc.com", dob:""  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia", email: "charly@abc.com", dob:""  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi", email: "jannuzi@abc.com", dob:"" }
+    ];
 
-        return api;
+    var api ={
+        registerUser: registerUser,
+        findUserById: findUserById,
+        findUserbyUsername: findUserbyUsername,
+        findUserByUsernameAndPassword : findUserByUsernameAndPassword,
+        updateUser: updateUser,
+        deleteUser:deleteUser
+    };
 
-        function registerUser(user) {
-            user._id = (new Date()).getTime() + "";
-            user.created = new Date();
-            users.push(user);
-            return user;
-        }
+    return api;
 
-        function findUserbyUsername(username) {
-            var user = users.find(function (user) {
-                return user.username === username;
-            });
-            if(typeof user === 'undefined'){
-                return null;
-            }
-            return user;
-        }
-
-        function findUserById(userId) {
-            for(var u in users){
-                if(users[u]._id === userId)
-                    return users[u];
-            }
-            return null;
-        }
-
-        function findUserByUsernameAndPassword(username, password) {
-            var found = false;
-            for(var u in users) {
-                var user = users[u];
-                if (user.username === username &&
-                    user.password === password) {
-
-                    return user;
-                }
-
-            }
-            return null;
-        }
-
-        function updateUser(userId, user) {
-            var user_old = findUserbyId(userId);
-            user_old.username = user.username;
-            user_old.password = user.password;
-            user_old.firstName = user.firstName;
-            user_old.lastName = user.lastName;
-            user_old.email = user.email;
-            user_old.dob = user.dob;
-        }
-
-        function deleteUser(userId) {
-            var user_old = findUserById(userId);
-            var index = users.indexOf(user_old);
-            users.splice(index, 1);
-        }
+    function registerUser(user) {
+        user._id = (new Date()).getTime() + "";
+        user.created = new Date();
+        users.push(user);
+        return user;
     }
+
+    function findUserbyUsername(username) {
+        var user = users.find(function (user) {
+            return user.username === username;
+        });
+        if(typeof user === 'undefined'){
+            return null;
+        }
+        return user;
+    }
+
+    function findUserById(userId) {
+        for(var u in users){
+            if(users[u]._id === userId)
+                return users[u];
+        }
+        return null;
+    }
+
+    function findUserByUsernameAndPassword(username, password) {
+        var found = false;
+        for(var u in users) {
+            var user = users[u];
+            if (user.username === username &&
+                user.password === password) {
+
+                return user;
+            }
+
+        }
+        return null;
+    }
+
+    function updateUser(userId, user) {
+        var user_old = findUserbyId(userId);
+        user_old.username = user.username;
+        user_old.password = user.password;
+        user_old.firstName = user.firstName;
+        user_old.lastName = user.lastName;
+        user_old.email = user.email;
+        user_old.dob = user.dob;
+    }
+
+    function deleteUser(userId) {
+        var user_old = findUserById(userId);
+        var index = users.indexOf(user_old);
+        users.splice(index, 1);
+    }
+}
 
 })();
