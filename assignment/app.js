6614ade7e3498b1d5daaa3c54523e4a885b30fb9
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-08-09 04:01:36 -0400

    app.js update

--- assignment/app.js
+++ assignment/app.js
@@ -1,24 +1,12 @@
-/**
- * Created by vaibhav on 27-07-2017.
- */
-var mongoose = require('mongoose');
-var connectionString = 'mongodb://127.0.0.1:27017/WebDevelopment'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds137139.mlab.com:37139/heroku_d64prf40'; // user yours
-}
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/WebDevelopment");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("connected")
+});
+mongoose.Promise=require("q").Promise;
 
-if(process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
-}
-
-mongoose.connect(connectionString);
 require('./services/user.service.server.js');
 require('./services/website.service.server');
 require('./services/page.service.server');
