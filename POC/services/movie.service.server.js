ad1c36795e73682185ad9541a88d9319f7b36ec0
vaibhavkarnam <karnamvaibhav360@gmail.com>
2017-08-11 22:31:12 -0400

    updates

--- POC/services/movie.service.server.js
+++ POC/services/movie.service.server.js
@@ -3,14 +3,14 @@
  */
 var app=require('../../express');
 
-var http =require('http');
+var https =require('https');
 
 
 app.get("/api/search/:movie",searchMovieByTitle);
 app.get("/api/id/:id",searchMovieByImdbId);
 
 
-function searchMovieByImdbId(req, res) {
+function searchMoviByImdbId(req, res) {
     var imdbID = req.params['id'];
     console.log("found");
     var url = "http://www.omdbapi.com/?i="+imdbID+"&apikey=509f6a23";
@@ -19,12 +19,54 @@ function searchMovieByImdbId(req, res) {
     });
 }
 
-function searchMovieByTitle(req, res) {
+function searchMovieBTitle(req, res) {
     var movieTitle = req.params['movie'];
-    var url = "http://www.omdbapi.com/?s="+movieTitle+"&apikey=509f6a23";
+    var url = "http://www.omdbapi.com/?s=" + movieTitle + "&apikey=509f6a23";
     return http.get(url, function (response) {
         res.send(response);
     });
 
-
 }
+
+    function searchMovieByTitle(req,res) {
+        var movieTitle = req.params['movie'];
+        var options ={
+            host:"www.omdbapi.com",
+            path:"?s="+movieTitle+"&apikey=509f6a23"
+
+        };
+        //console.log(options.path);
+        var callback = function (response) {
+            var str = '';
+            response.on('data', function (data) {
+                str += data;
+            });
+            response.on('end', function () {
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(str);
+            });
+        };
+        https.get(options,callback);
+    }
+
+    function searchMovieByImdbId(req,res) {
+        var imdbID = req.params['id'];
+        var options ={
+            host:"www.omdbapi.com",
+            path:"?s="+imdbID+"&apikey=509f6a23"
+
+        };
+        //console.log(options.path);
+        var callback = function (response) {
+            var str = '';
+            response.on('data', function (data) {
+                str += data;
+            });
+            response.on('end', function () {
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(str);
+            });
+        };
+        https.get(options,callback);
+    }
+
--- public/project/POC/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by vaibhav on 02-08-2017.
- */
-(function () {
-    angular
-        .module("omdbApp", ["ngRoute"]);
-})();
